b5d20223b81db21d908118a777feba195ed5afcd
unknown <adeelnasirkbw@gmail.com>
2022-08-29 06:16:25 -0700

    redux setup initialized

---
+++ client/src/redux/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, combineReducers, } from 'redux'
+
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+
+})
+
+export const store = createStore(rootReducer, {}, applyMiddleware(thunk))
